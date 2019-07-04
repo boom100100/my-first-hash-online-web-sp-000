a42cbeb9f8585979839aca7260b6e66c62167902
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 22:38:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -41,6 +41,7 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
+  return shipping_manifest
 
 
   # return the shipping_manifest hash below
